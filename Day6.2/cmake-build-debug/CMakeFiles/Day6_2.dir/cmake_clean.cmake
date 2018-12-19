8dea924d000f3ebd5a6759ba64cce7d2f4319824
laurens060606 <454961@student.saxion.nl>
2018-12-19 10:18:52 +0100

    Day6 finished

---
+++ Day6.2/cmake-build-debug/CMakeFiles/Day6_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Day6_2.dir/main.cpp.obj"
+  "Day6_2.pdb"
+  "Day6_2.exe"
+  "Day6_2.exe.manifest"
+  "libDay6_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Day6_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
